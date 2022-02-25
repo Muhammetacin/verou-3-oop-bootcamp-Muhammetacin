97b3c3bd23026b292af5187c0fbcec7fbf0ef3f4
Muhammet Cin <muhammetacin@gmail.com>
2022-02-25 11:13:08 +0100

    Add BREAKING before breaking news article.

--- UseCase4/Article.php
+++ UseCase4/Article.php
@@ -2,8 +2,19 @@
 
 class Article extends Content
 {
-    public function __construct($title, $text)
+    private bool $isBreakingNews;
+
+    public function __construct($title, $text, $isBreakingNews)
     {
         parent::__construct($title, $text);
+        $this->isBreakingNews = $isBreakingNews;
+    }
+
+    public function showTitle() : string
+    {
+        if($this->isBreakingNews) {
+            return 'BREAKING: ' . parent::showTitle();
+        }
+        return parent::showTitle();
     }
 }
